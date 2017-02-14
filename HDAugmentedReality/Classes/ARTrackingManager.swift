c90df3decd92e0c5e9a10cbf3a38cf914669e9cc
Alexander Heinrich <alexander@sn0wfreeze.de>
2017-02-14 11:11:28 +0100

    Added HDAugmentedReality Framework for Carthage usage

--- HDAugmentedReality/Classes/ARTrackingManager.swift
+++ HDAugmentedReality/Classes/ARTrackingManager.swift
@@ -178,21 +178,7 @@ open class ARTrackingManager: NSObject, CLLocationManagerDelegate
     
     open func locationManager(_ manager: CLLocationManager, didUpdateHeading newHeading: CLHeading)
     {
-        //Heading updated
-        if newHeading.headingAccuracy < 0 {
-            //Invalid heading
-            return
-        }
-        
-        var heading =  newHeading.trueHeading
-        //Use magnetic heading if true heading is not available
-        if heading < 0 {
-            heading = newHeading.magneticHeading
-        }
-        
-        //Modular of 360
-        heading = fmod(heading, 360.0)
-        self.heading = heading
+        self.heading = fmod(newHeading.trueHeading, 360.0)
     }
     
     open func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation])
