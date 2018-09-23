3f6dd058c39077b6684560e627fd58249f978959
Danijel Huis <danijelhuis@Danijels-MacBook-Pro.local>
2018-09-23 20:40:28 +0200

    Example project restructure

---
+++ Example/Pods/Target Support Files/HDAugmentedReality/HDAugmentedReality-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_HDAugmentedReality : NSObject
+@end
+@implementation PodsDummy_HDAugmentedReality
+@end
---
+++ Example/Pods/Target Support Files/Pods-HDAugmentedRealityDemo/Pods-HDAugmentedRealityDemo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HDAugmentedRealityDemo : NSObject
+@end
+@implementation PodsDummy_Pods_HDAugmentedRealityDemo
+@end
